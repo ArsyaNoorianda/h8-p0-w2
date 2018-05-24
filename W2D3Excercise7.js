975964d6253ad84dc1496074ae79426b41c80c99
ArsyaNoorianda <arsyha.noorianda@gmail.com>
2018-05-25 00:03:36 +0700

    pengumpulan looping asterix

---
+++ W2D3Excercise7.js
@@ -0,0 +1,38 @@
+console.log('1. Menyusun Barisan Bintang')
+
+
+console.log('Jika Rows 1 = 5:');
+for(var rows1 = 1; rows1 <= 5; rows1++){
+  console.log('*');
+}
+
+
+
+
+
+
+console.log('------------------------------------------------------------------')
+
+
+console.log('2. Menyusun Barisan Bintang Dengan Nested Looping')
+
+console.log('Jika Rows 2 = 5:');
+
+for(var rows2 = 1; rows2 <= 5; rows2++){
+  console.log('*****');
+}
+
+console.log('------------------------------------------------------------------')
+
+
+console.log('3. Menyusun Barisan Tangga Bintang Dengan Nested Looping')
+
+console.log('jika rows 3 = 5:')
+
+var sutaa = '*';
+for(var rows3 = 1; rows3 <= 5; rows3++)
+{
+  console.log(sutaa);
+  sutaa += '*';
+}
+
