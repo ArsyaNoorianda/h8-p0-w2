329d5b2cf51026d8ff7e83139d89baf6745b0c24
ArsyaNoorianda <arsyha.noorianda@gmail.com>
2018-05-27 20:51:56 +0700

    pertambahan agar lebih dinamis

--- W2D2Excercise3.js
+++ W2D2Excercise3.js
@@ -1,29 +1,29 @@
-var nama = 'Nina';     
-var peran = 'Ksatria';    
+var nama = 'Adnairoon';     
+var peran = 'Ksatria';  
 
 if (nama === '' && peran === ''){
   console.log('Nama harus diisi!') ;
  
 
-}else if (nama === 'Mikael' && peran === ''){
-  console.log('Halo Mikael, Pilih peranmu untuk memulai game!');
+}else if (peran === ''){
+  console.log('Pilih peranmu untuk memulai game!');
   
 
-}else if(nama === 'Nina' && peran === 'Ksatria'){
-  console.log('Selamat datang di Dunia Proxytia, Nina');
-  console.log('Halo ksatria Nina, kamu dapat menyerang dengan senjatamu!' );
+}else if(peran === 'Ksatria'){
+  console.log('Selamat datang di Dunia Proxytia,' +nama);
+console.log('Halo '+peran,nama+',' ,'kamu dapat menyerang dengan senjatamu!' );
 
 
-}else if(nama === 'Danu' && peran === 'Tabib'){
-    console.log('Selamat datang di Dunia Proxytia, Danu');
-    console.log('Halo Tabib Danu, kamu akan membantu temanmu yang terluka!' );  
+}else if(peran === 'Tabib'){
+    console.log('Selamat datang di Dunia Proxytia ' + nama);
+    console.log('Halo '+peran,nama+', kamu akan membantu temanmu yang terluka!' );  
 
-}else if(nama === 'Zero' && peran === 'Penyihir'){
-    console.log('Selamat datang di Dunia Proxytia, Zero');
-    console.log('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!' );  
+}else if(peran === 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxytia,' + nama);
+    console.log('Halo' +peran,nama+ ', ciptakan keajaiban yang membantu kemenanganmu!' );  
 
-}else if( nama !== ''  &&  peran === ''){
-  console.log("Halo "+ nama +", Pilih Peranmu untuk memulai game");
+
+
 }else{
-  console.log('Nama harus diisi!');
+  console.log('Pilih peran yang ADA WOY!!!');
 }
